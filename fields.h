de93e23b05d1fff120e6a147b49ab86700ee196a
Paul Stratton <paulstratton56@gmail.com>
2024-02-16 17:33:48 -0800

    Field-Constructors
    > Populated child fields with constructors and private vars
    > Created "demoFunctions" for easy access to testing functions
    >> Created "binToInt" and "strToVec" functions

--- fields.h
+++ fields.h
@@ -1,38 +1,96 @@
 #ifndef FIELDS_H
 #define FIELDS_H
 
+#include <string>
+#include <vector>
+
 class Field{
 public:
 
 
-private:
-
+protected:
 
 };
 
 class RField: public Field{
+public:
+    RField(int* binaryCode){
+        
+    }
+
+    void printInfo(){
+
+    }
+
+private:
+    string operation;
+    int rs1, rs2, rd, func3, func7;
+
 
 };
 
 class IField: public Field{
+public:
+    IField(int* binaryCode){
+
+    }
+
+    void printInfo(){
+
+    }
+
+private:
+    string operation;
+    int rs1, rd, imm;
 
 };
 
 class SField: public Field{
+public:
+    SField(int* binaryCode){
+
+    }
+
+    void printInfo(){
+
+    }
+
+private:
+    string operation;
+    int rs1, rd, imm;
 
 };
 
 class SBField: public Field{
+public:
+    SBField(int* binaryCode){
 
-};
+    }
 
-class UField: public Field{
+    void printInfo(){
+
+    }
+
+private:
+    string operation;
+    int rs1, rd, imm;
 
 };
 
 class UJField: public Field{
+public:
+    UJField(int* binaryCode){
+
+    }
+
+    void printInfo(){
+
+    }
+
+private:
+    string operation;
+    int rd, imm;
 
 };
-
 
 #endif

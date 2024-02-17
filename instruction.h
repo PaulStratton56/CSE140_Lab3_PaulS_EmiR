fe5069e06aadad046790e438b1dde34bf139e3d0
[emi rueth] <emi.seawater@gmail.com>
2024-02-16 18:03:22 -0800

    Adding most of public and private files

--- instruction.h
+++ instruction.h
@@ -5,10 +5,16 @@
 
 class Instruction{
 public:
+    Instruction(string Binary);     //Constructor
+    ~Instruction();                 //Destructor
 
-
+    void printInfo();               //Printing Information
 
 private:
+    string binaryText;
+    int* binary = new int[32];
+    Field* info;
+    void processBinaryText();       //Convert the Binary 
 
 
 
